aad7f8c0abe6c3522d26869721d56bb3cf60ab63
abid365 <abidibnazam@gmail.com>
2024-05-23 15:36:45 +0600

    mongoose validation done

--- src/app/modules/product.model.ts
+++ src/app/modules/product.model.ts
@@ -2,21 +2,30 @@ import { Schema, model } from 'mongoose';
 import { Product, Variant, Inventory } from './product/product.interface';
 
 const variantSchema = new Schema<Variant>({
-  type: { type: String },
-  value: { type: String },
+  type: { type: String, required: [true, 'Type is required'] },
+  value: { type: String, required: [true, 'Value is required'] },
 });
 
 const InventorySchema = new Schema<Inventory>({
-  quantity: { type: Number },
-  inStock: { type: Boolean },
+  quantity: { type: Number, required: [true, 'Quantity is required'] },
+  inStock: { type: Boolean, required: [true, 'Instock is required'] },
 });
 
 const productSchema = new Schema<Product>({
-  name: { type: String },
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true,
+    maxlength: [20, 'Name can not be more than 20 characters'],
+  },
   description: { type: String },
-  price: { type: Number },
-  category: { type: String },
-  tags: { type: [String] },
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+    min: [0, 'Too few phones'],
+  },
+  category: { type: String, required: [true, 'Category is required'] },
+  tags: { type: [String], required: [true, 'Tags are required'] },
   variants: [variantSchema],
   inventory: InventorySchema,
 });
